70079908e522609d18d4b065302b71c2697818b6
Lingxin Zhao <zhaolingxin@wustl.edu>
2017-03-02 14:47:33 -0600

    remove all files

--- Pods/Target Support Files/Pods-LingxinZhao_Lab4F/Pods-LingxinZhao_Lab4F-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LingxinZhao_Lab4F : NSObject
-@end
-@implementation PodsDummy_Pods_LingxinZhao_Lab4F
-@end
--- Pods/Target Support Files/Pods-LingxinZhao_Lab4FTests/Pods-LingxinZhao_Lab4FTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LingxinZhao_Lab4FTests : NSObject
-@end
-@implementation PodsDummy_Pods_LingxinZhao_Lab4FTests
-@end
